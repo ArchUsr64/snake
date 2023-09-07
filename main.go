4d78dfff14d0a4ade536a437da3747e70d2f85bd
Anshul <anshulusr@gmail.com>
2023-09-07 17:18:50 +0530

    Added border rendering to the grid

--- main.go
+++ main.go
@@ -13,6 +13,12 @@ type Game struct {
 }
 
 const CURSOR_HOME = "\033[H"
+const TOP_LEFT = "╭"
+const TOP_RIGHT = "╮"
+const BOTTOM_LEFT = "╰"
+const BOTTOM_RIGHT = "╯"
+const HORIZONTAL = "│"
+const VERTICAL = "─"
 
 func main() {
 	var game = Game{
@@ -27,9 +33,29 @@ func main() {
 
 func (game Game) render() {
 	fmt.Print(CURSOR_HOME)
-	for i := 0; i < game.grid_size.y; i++ {
-		for j := 0; j < 2*game.grid_size.x; j++ {
-			fmt.Print("@")
+	for i := 0; i < game.grid_size.y+2; i++ {
+		if i == 0 {
+			fmt.Print(TOP_LEFT)
+		} else if i == game.grid_size.y+1 {
+			fmt.Print(BOTTOM_LEFT)
+		} else {
+			fmt.Print(HORIZONTAL)
+		}
+		for j := 0; j < 2*game.grid_size.x+2; j++ {
+			if i == 0 || i == game.grid_size.y+1 {
+				fmt.Print(VERTICAL)
+			} else if j == 0 || j == 2*game.grid_size.x+1 {
+				fmt.Print(" ")
+			} else {
+				fmt.Print("@")
+			}
+		}
+		if i == 0 {
+			fmt.Print(TOP_RIGHT)
+		} else if i == game.grid_size.y+1 {
+			fmt.Print(BOTTOM_RIGHT)
+		} else {
+			fmt.Print(HORIZONTAL)
 		}
 		fmt.Println()
 	}
